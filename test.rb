9423b6ef893586fabd0c088bc1afef98fc9e3c28
ragu <ragu@sooraavaliSSD-ubuntu>
2014-08-24 20:36:10 +0530

    removed fakefs for unit tests

--- test.rb
+++ test.rb
@@ -1,38 +1,48 @@
-require './zero-files.rb'
-require './import-photos.rb'
+require "./zero-files.rb"
+require "./import-photos.rb"
 
-require 'rubygems'
-gem 'mocha'
+require "rubygems"
+gem "mocha"
 
-require 'test/unit'
-require 'mocha/test_unit'
-require 'fakefs'
+require "test/unit"
+require "mocha/test_unit"
 
 class ImageImportTest < Test::Unit::TestCase
-  ROOT = '/tmp/root/'
+  ROOT = "/tmp/root"
 
   def setup
-    FakeFS do
-      FileUtils.mkpath '/tmp/root/2015'
-      FileUtils.mkpath '/tmp/.cache/'
+    FileUtils.mkpath "#{ROOT}/2015"
+    FileUtils.mkpath "/tmp/.cache/"
 
-      FileUtils.mkpath '/tmp/root/.cache/'
-      FileUtils.mkpath '/tmp/root/2015/01/'
-      FileUtils.mkpath '/tmp/root/2017/05/'
+    FileUtils.mkpath "#{ROOT}/.cache/"
+    FileUtils.mkpath "#{ROOT}/2015/01/"
+    FileUtils.mkpath "#{ROOT}/2017/05/"
 
-      FileUtils.touch '/tmp/root/IMG_001.JPG'
-      FileUtils.touch '/tmp/root/IMG_002.JPG'
-      FileUtils.touch '/tmp/root/.cache/IMG_001.JPG'
-      FileUtils.touch '/tmp/root/.cache/IMG_002.JPG'
-      FileUtils.touch '/tmp/root/.cache/IMG_003.JPG'
+    FileUtils.touch "#{ROOT}/IMG_001.JPG"
+    FileUtils.touch "#{ROOT}/text.txt"
+    FileUtils.touch "#{ROOT}/text-.txt"
+    FileUtils.cp "./test/data/IMG_002.JPG", "#{ROOT}/IMG_002.JPG"
+    FileUtils.touch "#{ROOT}/.cache/IMG_001.JPG"
+    FileUtils.cp "./test/data/IMG_002.JPG", "#{ROOT}/.cache/IMG_002.JPG"
+    FileUtils.touch "#{ROOT}/.cache/IMG_003.JPG"
 
-      File.expects(:zero?).with('/tmp/root/IMG_001.JPG').returns(true)
-      File.expects(:zero?).with('/tmp/root/IMG_002.JPG').returns(false)
-    end 
+    File.expects(:zero?).with("#{ROOT}/IMG_001.JPG").returns(true)
+    File.expects(:zero?).with("#{ROOT}/IMG_002.JPG").returns(false)
+    File.expects(:zero?).with("#{ROOT}/text.txt").returns(false)
+    File.expects(:zero?).with("#{ROOT}/text-.txt").returns(true)
+  end
+
+  def teardown
+    FileUtils.rm_r ROOT
   end
 
   def test_files
     valid_files = files(ROOT)
-    assert_equal(['/tmp/root/IMG_002.JPG'], valid_files)
+    assert_equal(["#{ROOT}/IMG_002.JPG", "#{ROOT}/text.txt"].sort, valid_files.sort)
+  end
+
+  def test_images
+    images = images(ROOT)
+    assert_equal(["#{ROOT}/IMG_002.JPG"], images)
   end
 end
